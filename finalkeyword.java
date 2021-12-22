1d396125b491ce741d0312656126815979369724
duyhieuduy <hieu01648476512@gmail.com>
2021-12-22 09:24:36 +0700

    'a'

---
+++ finalkeyword.java
@@ -0,0 +1,7 @@
+public class finalkeyword {
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        // pi = 4; khi co final k the thay doi gia tri cua bien nua
+        System.out.println(pi);
+    }
+}
